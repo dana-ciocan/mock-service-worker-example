10ca6f3cd6d36f0da6317176ff6f0d2ddbe12e81
Dana Ciocan <danaciocan@gmail.com>
2026-06-25 19:10:14 +0100

    Set up very basic cat app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [catImage, setCatImage] = useState(null)
+  useEffect(() => {
+    fetch('https://api.thecatapi.com/v1/images/search')
+      .then(response => response.json())
+      .then(data => setCatImage(data[0].url))
+  }, [])
+
+  return (
+    <>
+      <section id="center">
+        <h1>A random cat</h1>
+        {catImage && <img src={catImage} alt="A cute cat" />}
+      </section>
+    </>
+  )
+}
+
+export default App
