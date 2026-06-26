248a7609afacf7bb3f511f7ba2eba3a66a0b8e45
Dana Ciocan <danaciocan@gmail.com>
2026-06-26 08:17:34 +0100

    Wrap the app with MockServiceWorkerProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { MockServiceWorkerProvider } from './MockServiceWorkerProvider'
 
 createRoot(document.getElementById('root')).render(
+  <MockServiceWorkerProvider>
     <App />
+  </MockServiceWorkerProvider>
 )
